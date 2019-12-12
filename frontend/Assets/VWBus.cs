4dff1aa8529df079f023d516cd1e68b58e48590c
Florian Ellinger <florian.ellinger@docufy.de>
2019-12-12 21:55:48 +0100

    add missing chewbacca

---
+++ frontend/Assets/VWBus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VWBus : MonoBehaviour
+{
+
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (MeshRenderer mr in GetComponentsInChildren<MeshRenderer>()){
+            mr.material = material;
+            mr.materials[0] = material;
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
